c8011d2fcba4e2d676341be53ea57218723ad2cf
Leo Sporn <spornleo@gmail.com>
2020-05-16 09:00:02 -0700

    - Leo: Re-introduced syntax analysis and content classification

--- backend/server.js
+++ backend/server.js
@@ -7,35 +7,35 @@ const PORT = 4000;
 app.use(cors());
 
 app.get("/", (req, res) => {
-    sendMessage();
-    res.send("hello");
+  sendMessage();
+  res.send("hello");
 });
 
 app.listen(PORT, () => {
-    console.log(`Server listening on PORT ${PORT}`);
+  console.log(`Server listening on PORT ${PORT}`);
 });
 
 const sendMessage = () => {
-    login({ email: "rhacks2020@gmail.com", password: "pw121234" }, (err, api) => {
-        if (err) return console.error(err);
-        let msg = "this is so cool";
-        // sendMessages();
-        api.getFriendsList((err, friends) => {
-            friends.forEach((friend) => {
-                console.log(friend.fullName);
-            });
-        });
+  login({ email: "rhacks2020@gmail.com", password: "pw121234" }, (err, api) => {
+    if (err) return console.error(err);
+    let msg = "this is so cool";
+    // sendMessages();
+    api.getFriendsList((err, friends) => {
+      friends.forEach((friend) => {
+        console.log(friend.fullName);
+      });
     });
+  });
 };
 
 const sendMessages = (msg) => {
-    let leoID = "1817870651";
-    let aidanID = "793724637";
-    let shaqilID = "570920248";
-    let myID = "100051348402858";
-    let ids = [leoID, aidanID, shaqilID, myID];
+  let leoID = "1817870651";
+  let aidanID = "793724637";
+  let shaqilID = "570920248";
+  let myID = "100051348402858";
+  let ids = [leoID, aidanID, shaqilID, myID];
 
-    for (id of ids) {
-        api.sendMessage(msg, id);
-    }
+  for (id of ids) {
+    api.sendMessage(msg, id);
+  }
 };
