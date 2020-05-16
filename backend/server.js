d922c86113d4080b69b6a246f25b81c2d99352ba
Eric Kim <erickim195@gmail.com>
2020-05-16 12:14:25 -0700

    post request working

--- backend/server.js
+++ backend/server.js
@@ -2,12 +2,23 @@ const express = require("express");
 const login = require("facebook-chat-api");
 const cors = require("cors");
 const app = express();
+const bodyParser = require("body-parser")
 const PORT = 4000;
 
 app.use(cors());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
 app.get("/", (_req, res) => {
-  sendMessage(["Eric Kim", "Shaqil Rahemtulla"], res.send("hello"));
+  res.send("hello");
+});
+
+app.post("/", (req, res) => {
+  let email = req.body.email;
+  let password = req.body.password;
+  let friends = req.body.friends;
+  console.log(`Email: ${email}, Password: ${password}, Friends: ${friends}`);
+  res.end("post request received");
 });
 
 app.listen(PORT, () => {
