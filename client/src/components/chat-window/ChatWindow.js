4c2afb770957782b6e59af388a40d2dc68c0ed31
Eric Kim <erickim195@gmail.com>
2020-05-16 20:49:11 -0700

    added chat window

--- client/src/components/chat-window/ChatWindow.js
+++ client/src/components/chat-window/ChatWindow.js
@@ -1,8 +1,19 @@
 import React, { useEffect, useState } from "react";
 import InputField from "../input-field/InputField";
 import axios from "axios";
+import { blue } from "@material-ui/core/colors";
 
 function ChatWindow({ email, password, friends }) {
+  const chatWindowStyles = {
+    backgroundColor: "white",
+    height: "50vh",
+    width: "87%",
+    maxWidth: 900,
+    marginBottom: 30,
+    border: "solid #CCCCCC 6px",
+    borderRadius: 15
+  }
+
   const sendFbMessage = () => {
     axios.post("http://localhost:4000", {
       email: email,
@@ -23,8 +34,10 @@ function ChatWindow({ email, password, friends }) {
   };
 
   return (
-    <div>
-      <h1> Hi </h1>
+    <div className="chat-window" style={chatWindowStyles}>
+      <div>
+
+      </div>
       {/* <InputField onClick={handleFieldChange} /> */}
     </div>
   );
