aec6a24e8881ea73637fa2876002a357d57b1dbe
Eric Kim <erickim195@gmail.com>
2020-05-16 20:01:47 -0700

    added Aidan's ChatWindow component

--- client/src/components/chat-window/ChatWindow.js
+++ client/src/components/chat-window/ChatWindow.js
@@ -1,11 +1,17 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import InputField from "../input-field/InputField";
+import axios from "axios";
 
 function ChatWindow(props) {
   const [values, setValues] = useState({});
   const handleFieldChange = (value) => {
-    setValues({ ...values, input: value });
-    console.log(values.input);
+    setValues({ ...values, input: value })
+    console.log(value);
+    axios.post("http://localhost:4000/query", { text: value })
+      .then((result) => {
+        setValues({ ...values, sentiment: result.data[0][0] });
+      });
+    console.log(values.sentiment);
   };
   const field = <InputField onClick={handleFieldChange} />;
   return <div>{field}</div>;
